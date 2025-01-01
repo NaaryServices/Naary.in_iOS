2b7fb7cc914cb394fe7a177f9491caab3d6f7bcc
Naary.in <naary-services@naary.in>
2025-01-01 14:59:31 +0530

    test commit to the git

---
+++ Naary/Naary/NaaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaaryApp.swift
+//  Naary
+//
+//  Created by Chandana Sudha Madhuri Kandari on 01/01/25.
+//
+
+import SwiftUI
+
+@main
+struct NaaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Naary/Naary/NaaryUI/BackButtonUI.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct BackButtonView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var body: some View {
+        Button(action: {
+            presentationMode.wrappedValue.dismiss() // Dismiss the view (Go back)
+        }) {
+            Image(systemName: "chevron.backward.circle.fill") // Back arrow icon
+                .foregroundColor(.white) // Customize back button color to black
+        }
+        .padding(.horizontal, 25)
+    }
+}
+
+#Preview {
+    BackButtonView()
+}
